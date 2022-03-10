e9e3d69703a890ebbff67057882d4d605197f278
Kumiho <pavleta.taseva@gmail.com>
2022-03-10 12:13:20 +0200

    Find a doctor page finished

--- src/components/FindDoctor/FindDoctor.js
+++ src/components/FindDoctor/FindDoctor.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation.js';
+import '../FindDoctor/FindDoctor.css';
+import Footer from '../Footer/Footer.js';
 
 function FindDoctor() {
-  return (
-    <div>
-        <Navigation />
-    </div>
-  )
+    return (
+        <section id="find-doctor-container">
+            <Navigation />
+            <div className="find-doctor-div">
+                <h2 id="find-doctor-h2">Find a Doctor</h2>
+                <div className="search-field">
+                    <form id="search-form">
+                        <div className="search-doctor">
+                        <label className="serach-label" for="search">Find your specialist:</label>
+                        <input id="search-input" type="search" placeholder="Use our search tool and enter the name of a medical person, location or specialty..." required />
+                        </div>
+                        <button id="searchBtn">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </section>
+    )
 }
 
 export default FindDoctor;
