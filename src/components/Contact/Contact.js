3ae40bf3c589e754eb919cbe6dd804ac2f0293bc
Kumiho <pavleta.taseva@gmail.com>
2022-03-10 14:56:20 +0200

    add Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contacts">
+        <h4>Contact Us</h4>
+        <p>1346, 1, Saint George Sofijski Blvd., Sofia</p>
+        <p>4000, 47, Petka Tarvska, Plovdiv</p>
+        <p>8000, 12, Bratya Miladinovi Blvd., Burgas</p>
+        <p>Tel. 0898 / 456371</p>
+        <p>Email: newlife.hospital@gmail.com</p>
+    </div>
+  )
+}
+
+export default Contact;
--- src/components/FindDoctor/FindDoctor.js
+++ src/components/FindDoctor/FindDoctor.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation.js';
 import '../FindDoctor/FindDoctor.css';
 import Footer from '../Footer/Footer.js';
+import Contact from '../Contact/Contact.js';
 
 function FindDoctor() {
     return (
@@ -12,12 +13,16 @@ function FindDoctor() {
                 <div className="search-field">
                     <form id="search-form">
                         <div className="search-doctor">
-                        <label className="serach-label" for="search">Find your specialist:</label>
-                        <input id="search-input" type="search" placeholder="Use our search tool and enter the name of a medical person, location or specialty..." required />
+                            <label className="serach-label" for="search">Find your specialist:</label>
+                            <input id="search-input" type="search" placeholder="Use our search tool and enter the name of a medical person, location or specialty..." required />
                         </div>
                         <button id="searchBtn">Submit</button>
                     </form>
                 </div>
+                <button className="requestBtn" type="submit" name="action">Request an appointment
+                    <i class="material-icons right">send</i>
+                </button>
+                <Contact />
             </div>
             <Footer />
         </section>
