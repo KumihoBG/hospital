60f0b4c50bc8793a0f09cd7c892d186b266c5728
Kumiho <pavleta.taseva@gmail.com>
2022-03-24 16:13:51 +0200

    request an appointment view finished

--- src/components/ReactCalendar/ReactCalendar.js
+++ src/components/ReactCalendar/ReactCalendar.js
@@ -3,12 +3,14 @@ import Calendar from 'react-calendar';
 
 function ReactCalendar() {
     const today = new Date()
-    let tomorrow =  new Date()
+    let tomorrow = new Date()
     tomorrow.setDate(today.getDate() + 1)
     const [date, setDate] = useState(tomorrow);
+    const [time, setTime] = useState();
     const [checkDate, setCheckDate] = useState(false);
     const [appointment, setAppointment] = useState('');
-        
+
+
     const onChange = date => {
         setDate(date);
         setCheckDate(date <= today);
@@ -17,14 +19,16 @@ function ReactCalendar() {
     }
 
     function checkDateForAppointment() {
-        console.log(checkDate);
-        if(!checkDate) {
+        if (!checkDate) {
             setAppointment(date.toLocaleDateString('en-US'));
-            console.log(appointment);
-        }    
+        }
     }
 
-    console.log(appointment);
+    async function createAppointment(event) {
+        event.preventDefault();
+        const hours = document.getElementById('appt').value;
+        setTime(hours)
+    }
 
     return (
         <>
@@ -32,24 +36,35 @@ function ReactCalendar() {
                 <Calendar
                     value={date}
                     onChange={onChange}
-                 />
+                />
             </div>
             <div className="date-info">
                 <h5>Scheduling Information</h5>
                 <p className='date-text'>Current date: {new Date().toLocaleDateString('en-US')}</p>
                 {checkDate
-                ? <div>
-                    <p className='date-text'>Your desired date cannot be set in the past. Please, choose another date.</p>
-                    <p className='date-text'>What do you think about <span className='selected'>{tomorrow.toLocaleDateString('en-US')}</span>?</p>
+                    ? <div>
+                        <p className='date-text'><span className='selected'>Your desired date cannot be set for today or in the past. Please, choose another date.</span></p>
+                        <p className='date-text'>What do you think about <span className='selected'>{tomorrow.toLocaleDateString('en-US')}</span>?</p>
                     </div>
-                : <div>
-                    {!appointment
-                    ? <div><p className='date-text'>Select appointment date from the calendar above: <span className='selected'>{appointment}</span></p></div>
-                    : <div><p className='date-text'>Current appointment date: <span className='selected'>{date.toLocaleDateString('en-Us')}</span></p>
+                    : <div>
+                        {!appointment
+                            ? <div><p className='date-text'>Select appointment date from the calendar above: <span className='selected'>{appointment}</span></p></div>
+                            : <div><p className='date-text'>Current appointment date: <span className='selected'>{date.toLocaleDateString('en-Us')}</span></p>
+                            </div>
+                        }
                     </div>
-                    }
-                </div>
                 }
+                <div className="hours-container">
+                    <label htmlFor="appt">Choose a time for your appointment:</label>
+                    <input type="time" id="appt" name="appt"
+                        min="10:00" max="19:00" required />
+                    <small>Office hours are 10am to 7pm</small>
+                </div>
+                <button onClick={(event) => { createAppointment(event) }} type="submit" id='checkAppointmentBtn'>Request and appointment</button>
+
+                <div className="final-info">
+                    <h4>Your appointment is scheduled for {appointment} at {time} o'clock.</h4>
+                </div>
             </div>
         </>
 
